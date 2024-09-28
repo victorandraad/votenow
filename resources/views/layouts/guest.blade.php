e2841e60977027a3b3d54de5960b0133f4d3da01
Sérgio Jr. <84044055+sergiojunior13@users.noreply.github.com>
2024-09-27 21:37:56 -0300

    feat(style): styled enter vote room page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Vote Now') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,16 +16,10 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 p-6 py-24">
-        <div>
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </div>
+<body class="font-sans text-gray-900 antialiased flex flex-col min-h-screen justify-start items-center py-10">
+    <x-application-logo class="w-40" />
 
-        {{ $slot }}
-    </div>
+    {{ $slot }}
 </body>
 
 </html>
--- resources/views/votes/enter_room.blade.php
+++ resources/views/votes/enter_room.blade.php
@@ -1,20 +1,14 @@
 <x-guest-layout>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <h2 class="text-2xl font-bold mb-4">Enter Voting Room</h2>
-            <form method="POST" action="{{ route('votes.join') }}">
-                @csrf
-                <div>
-                    <x-input-label for="code" :value="__('Room Code')" />
-                    <x-text-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code')" required autofocus />
-                    <x-input-error :messages="$errors->get('code')" class="mt-2" />
-                </div>
-                <div class="flex items-center justify-end mt-4">
-                    <x-primary-button class="ml-3">
-                        {{ __('Enter Room') }}
-                    </x-primary-button>
-                </div>
-            </form>
-        </div>
+    <div class="grow flex flex-col justify-center items-center">
+        <h2 class="text-3xl font-bold mb-10">Enter Voting Room</h2>
+        <form method="POST" action="{{ route('votes.join') }}">
+            @csrf
+                <x-text-input id="code" class="block mt-1 w-full text-center !py-2" type="text" name="code" :value="old('code')" placeholder="ENTER ROOM CODE" required
+                    autofocus />
+                <x-input-error :messages="$errors->get('code')" class="mt-2 text-center" />
+                <x-primary-button class="mt-2 py-3">
+                    {{ __('Enter Room') }}
+                </x-primary-button>
+        </form>
     </div>
 </x-guest-layout>
