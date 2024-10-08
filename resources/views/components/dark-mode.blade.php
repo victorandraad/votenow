a12b21c9cd1711f94f2c9c5f4275b315c55b7f43
Victor Daniel Andrade da Silva <68758638+victorandraad@users.noreply.github.com>
2024-10-07 21:59:00 -0300

    Add files via upload

---
+++ resources/views/components/dark-mode.blade.php
@@ -0,0 +1,4 @@
+<button id="themeButton" onclick="mudarCor()" {{ $attributes->merge(['class' => 'absolute h-10 w-10 md:h-12 md:w-12 max-md:top-16 right-6 rounded-full bg-white shadow-md shadow-gray-300']) }}>
+    <img id="buttonIcon" src="https://static.thenounproject.com/png/4802375-200.png" alt="" class="">
+</button>
+
